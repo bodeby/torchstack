9af55e563fc23903e2b6e885a4f17f23311236e5
Frederik Bode <thorbensen@gmail.com>
2024-07-11 18:23:31 +0200

    examples: working on text gen example

--- src/consensus/layer.py
+++ src/consensus/layer.py
@@ -41,9 +41,7 @@ class Layer:
             logits = outputs.logits
 
         probabilities = F.softmax(logits, dim=-1)  # Convert logits to probabilities
-        last_token_probabilities = probabilities[
-            0, -1, :
-        ]  # Get the probabilities for the last token
+        last_token_probabilities = probabilities[0, -1, :]  # Get the probabilities for the last token
 
         # Convert probabilities to a more readable format
         probs = last_token_probabilities.cpu().numpy()
