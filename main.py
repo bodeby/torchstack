987837914ef22fad93c3cef5ef09d3255a5ae1b9
bodeby <thorbensen@gmail.com>
2024-11-17 23:20:37 +0100

    chore: setup exports for base classes in __init__.py

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
-from consensus.configuration import Configuration
-from consensus.member import AutoModelMember
-from consensus.ensemble import Ensemble
-
-import consensus as co
-
+from consensus import AutoModelMember
+from consensus import Configuration
+from consensus import Ensemble
 
 def main():
-    config = Configuration(temperature=0.7)
-
     # Specialized Ensemble Member extending: AutoModelForCausalLM (transformers)
-    m1 = AutoModelMember(weight=0.5).from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
-    m2 = AutoModelMember(weight=0.5).from_pretrained("Qwen/Qwen2.5-3B-Instruct")
+    # m1 = AutoModelMember(weight=0.5).from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
+    # m2 = AutoModelMember(weight=0.5).from_pretrained("Qwen/Qwen2.5-3B-Instruct")
 
-    ensemble = Ensemble(config=config)
+    c = Configuration(temperature=0.7)
+    ensemble = Ensemble(config=c)
 
     # add ensemble members
-    ensemble.add_member(m1)
-    ensemble.add_member(m2)
+    #ensemble.add_member(m1)
+    # ensemble.add_member(m2)
 
-    # add
-    ensemble.build_vocabulary()
-
-    print("hello world", config)
-
-    # device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    # sentence = "Hello World!"
-    # tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased")
-    # model = BertModel.from_pretrained("bert-large-uncased")
-
-    # inputs = tokenizer(sentence, return_tensors="pt").to(device)
-    # model = model.to(device)
-    # outputs = model(**inputs)
-
+    print("hello world")
 
 if __name__ == "__main__":
     main()
---
+++ src/consensus/__init__.py
@@ -0,0 +1,10 @@
+from .configuration import Configuration
+from .member import AutoModelMember
+from .ensemble import Ensemble
+
+# Define the public API
+__all__ = [
+    "Configuration", 
+    "AutoModelMember", 
+    "Ensemble"
+    ]
