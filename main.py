58c5c3ffd0a62b12b49df5cc48403b9c8341e84b
bodeby <thorbensen@gmail.com>
2024-11-25 19:16:19 +0100

    feat[union_vocab]: crude implementation of vocabulary union

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from torch import Tensor
 import numpy as np
 
 from consensus import AutoModelMember
-from consensus import Tokenizer
+from consensus import Tokenizer # todo: since AutoTokenizer is not inheritable just use base. 
 from consensus import Configuration
 from consensus import Ensemble
 
@@ -28,25 +28,36 @@ def create_union_vocabulary(tokenizers: List[Tokenizer]):
     return sorted_union_vocab, union_vocab_index
 
 
-def create_tokenizer_mapping(tokenizer: Tokenizer, vocab: Tensor, union_vocab_index):
+def create_tokenizer_mapping(tokenizer: Tokenizer, vocab: Tensor, index):
     # grab vocabulary from tokenizer
     tk_vocab = tokenizer.get_vocab()
 
     # Step 4.1: Create mapping matrices for tokenizer 1
     tk_to_union = np.zeros((len(tk_vocab), len(vocab)), dtype=int)
     for token, t1_idx in tk_vocab.items():
-        union_idx = union_vocab_index[token]
+        union_idx = index[token]
         tk_to_union[t1_idx, union_idx] = 1
 
 
 def main():
-    # Specialized Ensemble Member extending: AutoModelForCausalLM (transformers)
+    # Setup specialized Ensemble Member extending: AutoModelForCausalLM (transformers)
     m1 = AutoModelMember.from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
     m2 = AutoModelMember.from_pretrained("Qwen/Qwen2.5-3B-Instruct")
 
-    # Specialized Ensemle Tokenizers extending: AutoTokenizer (transformers)
+    # Setup specialized Ensemle Tokenizers extending: AutoTokenizer (transformers)
     t1 = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
     t2 = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-3B-Instruct")
+
+    # Create common vocabulary
+    union_vocab, union_vocab_index = create_union_vocabulary([t1, t2])
+
+    t1_mapping = create_tokenizer_mapping(
+        tokenizer=t1, vocab=union_vocab, index=union_vocab_index
+    )
+
+    t2_mapping = create_tokenizer_mapping(
+        tokenizer=t2, vocab=union_vocab, index=union_vocab_index
+    )
 
     config = Configuration(temperature=0.7)
     # strategy = Strategy()
